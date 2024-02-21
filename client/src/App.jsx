e0cfd04d0a71b32ddc73d1ee82f0c5ecdac97b45
Ryan Pham <ryanlepham9@gmail.com>
2024-02-20 22:16:09 -0500

    Added Home Route and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import React, { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+
+import Home from "./pages/Home/Home";
+import Footer from "./pages/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+        <Footer />
+    </Router>
   );
 }
 
