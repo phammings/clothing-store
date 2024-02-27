25c7358407e8b96a2c39ffbf63c2b26dbf369f32
Ryan Pham <ryanlepham9@gmail.com>
2024-02-27 15:43:16 -0500

    Added Navbar to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import React, { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css';
 
 import Home from "./pages/Home/Home";
-import Footer from "./pages/Footer/Footer";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
