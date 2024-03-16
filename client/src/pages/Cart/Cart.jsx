426efb71d704e8df86f30bbad0f0b1b0039c69b5
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 16:48:07 -0400

    Added useEffect and useDispatch for cart handling products

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { Button, Col, Row, Typography } from "antd";
 import { Link } from "react-router-dom";
 
 import ContentTitle from "../../components/ContentTitle/ContentTitle";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
-
+import { selectCartItems, selectIsCartLoading } from "../../redux-toolkit/cart/cart-selector";
+import { fetchCart } from "../../redux-toolkit/cart/cart-thunks";
+import {
+    calculateCartPrice,
+    removeClothById,
+    resetCartState,
+    setCartItemsCount
+} from "../../redux-toolkit/cart/cart-slice";
 import CartItem from "./CartItem/CartItem";
 import Spinner from "../../components/Spinner/Spinner";
 import CartTotalPrice from "./CartTotalPrice";
@@ -13,7 +21,49 @@ import { ORDER } from "../../constants/routeConstants";
 import "./Cart.css";
 
 const Cart = () => {
-   
+    const dispatch = useDispatch();
+    const clothes = useSelector(selectCartItems);
+    const isCartLoading = useSelector(selectIsCartLoading);
+    const [clothInCart, setClothInCart] = useState(() => new Map());
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        const clothesFromLocalStorage = new Map(
+            JSON.parse(localStorage.getItem("clothes"))
+        );
+
+        dispatch(fetchCart(Array.from(clothesFromLocalStorage.keys())));
+        clothesFromLocalStorage.forEach((value, key) => {
+            setClothInCart(clothInCart.set(key, value));
+        });
+
+        return () => {
+            dispatch(resetCartState());
+        };
+    }, []);
+
+    const deleteFromCart = (clothId) => {
+        clothInCart.delete(clothId);
+
+        if (clothInCart.size === 0) {
+            localStorage.removeItem("clothes");
+            setClothInCart(new Map());
+        } else {
+            localStorage.setItem("clothes", JSON.stringify(Array.from(clothInCart.entries())));
+        }
+        dispatch(removeClothById(clothId));
+        dispatch(setCartItemsCount(clothInCart.size));
+    };
+
+    const onChangeClothItemCount = (clothId, inputValue) => {
+        setClothes(clothId, inputValue);
+        dispatch(calculateCartPrice(clothes));
+    };
+
+    const setClothes = (clothId, clothCount) => {
+        setClothInCart(clothInCart.set(clothId, clothCount));
+        localStorage.setItem("clothes", JSON.stringify(Array.from(clothInCart.entries())));
+    };
 
     return (
         <ContentWrapper>
