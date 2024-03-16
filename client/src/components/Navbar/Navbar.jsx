f8f4b177b53f89491638300b6e28a219e437174c
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 17:09:17 -0400

    Refactored Navbar to include Login, Registration and Account (adding soon)

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Badge } from "antd";
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import { Badge, Space } from "antd";
+
+import { selectUserFromUserState } from "../../redux-toolkit/user/user-selector";
+import { selectCartItemsCount } from "../../redux-toolkit/cart/cart-selector";
+import { logoutSuccess } from "../../redux-toolkit/user/user-slice";
+import { ACCOUNT, BASE, LOGIN, MENU, REGISTRATION } from "../../constants/routeConstants";
+import { CART } from "../../constants/urlConstants";
+import "./NavBar.css";
 
 const NavBar = () => {
+    const dispatch = useDispatch();
+    const usersData = useSelector(selectUserFromUserState);
+    const cartItemsCount = useSelector(selectCartItemsCount);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        dispatch(logoutSuccess());
+    };
+
+    const renderAuthLinks = () => {
+        if (usersData) {
+            return (
+                <>
+                    <li>
+                        <Link to={ACCOUNT}>
+                            <Space align="baseline">
+                                My Account
+                            </Space>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link id={"handleLogout"} to={BASE} onClick={handleLogout}>
+                            <Space align="baseline">
+                                Exit
+                            </Space>
+                        </Link>
+                    </li>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <li>
+                        <Link to={LOGIN}>
+                            <Space align="baseline">
+                                Sign In
+                            </Space>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={REGISTRATION}>
+                            <Space align="baseline">
+                                Sign Up
+                            </Space>
+                        </Link>
+                    </li>
+                </>
+            );
+        }
+    };
+
     return (
         <>
             <div className={"navbar-logo-wrapper"}>
-                <Link to={"/"}>
+                <Link to={BASE}>
                     <h1 className={"navbar-logo"} >
                         Clothes.
                     </h1>
@@ -18,18 +76,19 @@ const NavBar = () => {
                 <div>
                     <ul style={{ display: 'flex', listStyle: 'none', margin: '0 0 0 30px', padding: 0 }}>
                         <li>
-                            <Link to={"/"}>Home</Link>
+                            <Link to={BASE}>Home</Link>
                         </li>
                         <li>
-                            <Link to={{ pathname: "/"}}>Clothes</Link>
+                            <Link to={{ pathname: MENU, state: { id: "all" } }}>Clothes</Link>
                         </li>
                     </ul>
                 </div>
                 <div>
                     <ul style={{ display: 'flex', listStyle: 'none', margin: '0 30px 0 0', padding: 0 }}>
+                        {renderAuthLinks()}
                         <li className={"navbar-cart"} style={{margin: '13px 0 0 30px'}}>
-                            <Badge size="small" color={"red"}>
-                                <Link to={"/"}>
+                            <Badge count={cartItemsCount} size="small" color={"red"}>
+                                <Link to={CART}>
                                     <ShoppingCartOutlined />
                                 </Link>
                             </Badge>
