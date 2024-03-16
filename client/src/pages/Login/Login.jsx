f782b10df0b8cc8adc8e392b99657c318a4b8453
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 16:42:07 -0400

    Added useEffect for user login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Alert, Col, Divider, Form, Row, Space } from "antd";
 import { LockOutlined, MailOutlined } from "@ant-design/icons";
 
+import { selectErrorMessage } from "../../redux-toolkit/auth/auth-selector";
+import { resetAuthState } from "../../redux-toolkit/auth/auth-slice";
+import { activateAccount, login } from "../../redux-toolkit/auth/auth-thunks";
+import { selectSuccessMessage } from "../../redux-toolkit/user/user-selector";
 import { FORGOT } from "../../constants/routeConstants";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import ContentTitle from "../../components/ContentTitle/ContentTitle";
@@ -12,6 +16,27 @@ import IconButton from "../../components/IconButton/IconButton";
 import "./Login.css";
 
 const Login = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const params = useParams();
+    const errorMessage = useSelector(selectErrorMessage);
+    const successMessage = useSelector(selectSuccessMessage);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        
+        if (params.code) {
+            dispatch(activateAccount(params.code));
+        }
+
+        return () => {
+            dispatch(resetAuthState());
+        };
+    }, []);
+
+    const onClickSignIn = (userData) => {
+        dispatch(login({ userData, history }));
+    };
 
     return (
         <ContentWrapper>
