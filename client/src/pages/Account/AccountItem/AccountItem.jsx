f1381be058940365d40fe8085c96421438528244
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 17:30:51 -0400

    Added AccountItem compoenent

---
+++ client/src/pages/Account/AccountItem/AccountItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Typography } from "antd";
+
+import { selectIsUserLoading, selectUserFromUserState } from "../../../redux-toolkit/user/user-selector";
+import Spinner from "../../../components/Spinner/Spinner";
+
+const AccountItem = () => {
+    const usersData = useSelector(selectUserFromUserState);
+    const loading = useSelector(selectIsUserLoading);
+
+    return (
+        <>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <Typography.Title level={5} style={{ textAlign: "center" }}>
+                    Hello {usersData?.firstName} {usersData?.lastName}!
+                </Typography.Title>
+            )}
+        </>
+    );
+};
+
+export default AccountItem;
