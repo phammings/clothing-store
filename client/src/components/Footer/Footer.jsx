1c1a19b3f6fd7046d917e5161b778193abe76b5b
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 17:19:23 -0400

    Refactored code

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-
-import "./Footer.css";
+import { GithubOutlined, LinkedinOutlined, HomeFilled } from "@ant-design/icons";
+import './Footer.css';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <div className="footer">
             <div className="footer-social" style={{ textAlign: "center" }}>
-                <a href="https://www.linkedin.com/in/ryan-pham-b102051a2/" target="_blank">
-                    LinkedIn
+            <a href="https://www.linkedin.com/in/ryan-pham-b102051a2/" target="_blank">
+                    <LinkedinOutlined style={{ fontSize: "30px", color: "#ffffff"}} />
                 </a>
                 <a href="https://github.com/phammings?tab=repositories" target="_blank">
-                    Github
+                    <GithubOutlined style={{ fontSize: "30px", color: "#ffffff"}} />
                 </a>
                 <a href="https://phammings.github.io/my-portfolio" target="_blank">
-                    Portfolio
+                    <HomeFilled style={{ fontSize: "30px", color: "#ffffff"}} />
                 </a>
             </div>
             <div style={{ color: "#ffffff", textAlign: "center", marginTop: "32px" }}>
