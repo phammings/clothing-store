88cb2c12cbd48573256de7299a7f3ebb9c79cf47
Ryan Pham <ryanlepham9@gmail.com>
2024-03-16 16:56:30 -0400

    Created constants file to organize code structure better

---
+++ client/src/constants/routeConstants.js
@@ -0,0 +1,10 @@
+export const BASE = "/";
+export const LOGIN = "/login";
+export const REGISTRATION = "/registration";
+export const FORGOT = "/forgot";
+export const RESET = "/reset";
+export const ACTIVATE = "/activate";
+export const MENU = "/menu";
+export const PRODUCT = "/product";
+export const CONTACTS = "/contacts";
+
