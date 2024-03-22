0fa613ca8c58181152fc33a5b2f11e2b29cf3ee5
Ryan Pham <ryanlepham9@gmail.com>
2024-03-22 18:15:24 -0400

    Refactored code structure for more clarity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import Clothes from "./pages/Clothes/Clothes";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="*" element={<Home />} />
+        <Route exact path={"/clothes"} component={Clothes} />
       </Routes>
       <Footer />
     </Router>
