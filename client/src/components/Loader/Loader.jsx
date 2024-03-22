1234099f85f03b779237618ffa51f6662c0289fc
Ryan Pham <ryanlepham9@gmail.com>
2024-03-22 18:35:16 -0400

    Deleted redundant compoenent loader

--- client/src/components/Loader/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Spin } from "antd";
-
-const Spinner = () => {
-    return <Spin size={"large"} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '100%', height: '100%' }} />;
-};
-
-export default Spinner;
