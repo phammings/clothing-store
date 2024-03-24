0249d606c98d9ccc2d33e7ef302b76cc9825c93c
Ryan Pham <ryanlepham9@gmail.com>
2024-03-23 23:09:39 -0400

    Created User and Review repos

---
+++ server/src/main/java/com/clothingstore/repo/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.clothingstore.repo;
+
+import com.clothingstore.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
