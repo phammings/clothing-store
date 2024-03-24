615a055d73daadd1dc5bb79bdfd28eecafc90930
Ryan Pham <ryanlepham9@gmail.com>
2024-03-23 22:56:02 -0400

    Created OrderItem model

---
+++ server/src/main/java/com/clothingstore/entity/OrderItem.java
@@ -0,0 +1,44 @@
+package com.clothingstore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_item_seq")
+    @SequenceGenerator(name = "order_item_seq", sequenceName = "order_item_seq", initialValue = 12, allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "amount")
+    private Double amount;
+
+    @Column(name = "quantity")
+    private Long quantity;
+
+    @OneToOne
+    private Cloth cloth;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
