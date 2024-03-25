32d46d43aa4ce74d3afb6211e1be6e06d1694f09
Ryan Pham <ryanlepham9@gmail.com>
2024-03-24 21:45:38 -0400

    Added User Details/Principle implications

---
+++ server/src/main/java/com/clothingstore/configurations/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.clothingstore.configurations.security;
+
+import com.clothingstore.entity.User;
+import com.clothingstore.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.clothingstore.consts.ErrorMessage.USER_NOT_FOUND;
+
+
+@Service("userDetailsServiceImpl")
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+        if (user.getActivationCode() != null) {
+            throw new LockedException("Email not activated");
+        }
+        return UserPrincipal.create(user);
+    }
+}
