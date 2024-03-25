ea552e03cbb87806df20d704908a7df08c77a332
Ryan Pham <ryanlepham9@gmail.com>
2024-03-24 21:41:58 -0400

    Created web configs

---
+++ server/src/main/java/com/clothingstore/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.clothingstore.configurations;
+
+import com.clothingstore.configurations.security.JwtConfigurer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final JwtConfigurer jwtConfigurer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/auth/**",
+                        "/api/v1/auth/login",
+                        "/api/v1/registration/**",
+                        "/api/v1/clothes/**",
+                        "/api/v1/users/cart",
+                        "/api/v1/order/**",
+                        "/api/v1/review/**",
+                        "/websocket", "/websocket/**",
+                        "/img/**",
+                        "/static/**")
+                .permitAll()
+                .antMatchers("/auth/**", "/oauth2/**", "/**/*swagger*/**", "/v2/api-docs").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(jwtConfigurer);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
