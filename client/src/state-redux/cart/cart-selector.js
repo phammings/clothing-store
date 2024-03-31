874e94504bcaec57c741274978ca54b484bf179e
phammings <79019254+phammings@users.noreply.github.com>
2024-03-31 17:57:44 -0400

    Revert "Feature client updated navbar"

--- client/src/state-redux/cart/cart-selector.js
+++
@@ -1,7 +0,0 @@
-import { LoadingStatus } from "../../constants/types/types";
-
-export const selectCartState = (state) => state.cart;
-export const selectTotalPrice = (state) => selectCartState(state).totalPrice;
-export const selectCartItemsCount = (state) => selectCartState(state).cartItemsCount;
-export const selectCartItems = (state) => selectCartState(state).clothes;
-export const selectIsCartLoading = (state) => selectCartState(state).loadingState === LoadingStatus.LOADING;
--- client/src/state-redux/cart/cart-thunks.js
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-
-import RequestService from "../../security/request-service";
-import { USERS_CART } from "../../constants/urlConstants";
-
-export const fetchCart = createAsyncThunk("cart/fetchCart", async (clothIds) => {
-    const response = await RequestService.post(USERS_CART, clothIds);
-    return response.data;
-});
