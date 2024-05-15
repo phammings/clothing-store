d56423553e39c7846c2d25f27ebe69606bf495cd
Ryan Pham <ryanlepham9@gmail.com>
2024-05-15 12:32:01 -0400

    Updated constants urls

--- client/src/constants/routeConstants.js
+++ client/src/constants/routeConstants.js
@@ -10,12 +10,14 @@ export const CONTACTS = "/contacts";
 export const CART = "/cart";
 export const ORDER = "/order";
 export const ORDER_FINALIZE = "/order/finalize";
-export const ACCOUNT_ADMIN_ADD = "/account/admin/add";
-export const ACCOUNT_ADMIN_CLOTHES = "/account/admin/clothes";
-export const ACCOUNT_ADMIN_ORDERS = "/account/admin/orders";
-export const ACCOUNT_ADMIN_USERS = "/account/admin/users";
+export const OAUTH2_REDIRECT = "/oauth2/redirect";
+
 export const ACCOUNT = "/account";
 export const ACCOUNT_USER_INFO = "/account/user/info";
 export const ACCOUNT_USER_EDIT = "/account/user/edit";
 export const ACCOUNT_USER_ORDERS = "/account/user/orders";
 
+export const ACCOUNT_ADMIN_ADD = "/account/admin/add";
+export const ACCOUNT_ADMIN_CLOTHES = "/account/admin/clothes";
+export const ACCOUNT_ADMIN_ORDERS = "/account/admin/orders";
+export const ACCOUNT_ADMIN_USERS = "/account/admin/users";
--- client/src/constants/urlConstants.js
+++ client/src/constants/urlConstants.js
@@ -7,22 +7,7 @@ export const REGISTRATION = "/auth/registration";
 export const CART = "/cart";
 export const CLOTHES = "/clothes";
 
-export const ORDER = "/order";
-export const ORDER_GRAPHQL = "/order/graphql";
-
-export const REVIEW = "/review";
-
-export const USERS = "/users";
-export const USERS_CART = "/users/cart";
-export const USERS_GRAPHQL = "/users/graphql";
-
-export const CLOTHES_IDS = "/clothes/ids";
-export const CLOTH_SEARCH = "/clothes/search";
-export const CLOTH_SEARCH_TEXT = "/clothes/search/text";
-export const CLOTHES_GRAPHQL_CLOTH = "/clothes/graphql/cloth";
-export const CLOTHES_GRAPHQL_CLOTHES = "/clothes/graphql/clothes";
-export const CLOTHES_GRAPHQL_IDS = "/clothes/graphql/ids";
-
+// admin
 export const ADMIN_ADD = "/admin/clothes/add";
 export const ADMIN_EDIT = "/admin/clothes/edit";
 export const ADMIN_DELETE = "/admin/clothes/delete";
@@ -35,11 +20,29 @@ export const ADMIN_GRAPHQL_USER_ALL = "/admin/graphql/user/all";
 export const ADMIN_GRAPHQL_ORDERS = "/admin/graphql/orders";
 export const ADMIN_GRAPHQL_ORDER = "/admin/graphql/order";
 
+// auth
 export const AUTH_LOGIN = "/auth/login";
 export const AUTH_FORGOT = "/auth/forgot";
 export const AUTH_RESET = "/auth/reset";
 export const AUTH_EDIT_PASSWORD = "/auth/edit/password";
 export const REGISTRATION_ACTIVATE = "/registration/activate";
 
+// order
+export const ORDER = "/order";
+export const ORDER_GRAPHQL = "/order/graphql";
 
+// review
+export const REVIEW = "/review";
 
+// user
+export const USERS = "/users";
+export const USERS_CART = "/users/cart";
+export const USERS_GRAPHQL = "/users/graphql";
+
+// clothes
+export const CLOTHES_IDS = "/clothes/ids";
+export const CLOTH_SEARCH = "/clothes/search";
+export const CLOTH_SEARCH_TEXT = "/clothes/search/text";
+export const CLOTHES_GRAPHQL_CLOTH = "/clothes/graphql/cloth";
+export const CLOTHES_GRAPHQL_CLOTHES = "/clothes/graphql/clothes";
+export const CLOTHES_GRAPHQL_IDS = "/clothes/graphql/ids";
