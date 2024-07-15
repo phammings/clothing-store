be5caedbefd57d7f9c58478cb6fbd55e96ad8d42
Ryan Pham <ryanlepham9@gmail.com>
2024-07-14 22:31:01 -0400

    Fixed broken video homepage

--- client/src/components/pages/Home/Video/Video.jsx
+++ client/src/components/pages/Home/Video/Video.jsx
@@ -9,7 +9,7 @@ const Video = () => {
         <Link to={{ pathname: MENU, state: { id: "all" } }}>
             <div>
                 <video id="vid" autoPlay muted loop style={{ maxWidth: "100%", maxHeight: "100%", margin: "0 auto", display: "block" }}>
-                    <source src="./Video.mp4" type="video/mp4" />
+                    <source src="https://cdn.shopify.com/videos/c/vp/b0fcca84b1384940b7b77edcd7d93361/b0fcca84b1384940b7b77edcd7d93361.HD-1080p-7.2Mbps-19259948.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
                 <script>
